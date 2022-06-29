7bd6d1d716c20333eebf9e353d218512d8625acb
Rafael Domingues <rafatomas@gmail.com>
2022-06-29 20:48:41 -0300

    readme
    Readme page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -27,11 +27,13 @@ export function Subscribe() {
   }
 
   return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="mx-w-[640px]">
-          <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex items-center flex-col">
+      <div className="w-full max-w-[1100px] flex flex-col-reverse p-4 md:p-0 md:items-center md:flex-row md:justify-between md:mt-20 md:mx-auto">
+        <div className="mx-w-[640px] p-4 text-center md:text-left">
+          <div className="flex justify-center mt-2 md:justify-start">
+            <Logo />
+          </div>
+          <h1 className="mt-8 text-[2.5rem] leading-tight xs:text-[1.5rem]">
             Build an <strong className="text-blue-500">app from scratch</strong>{" "}
             with <strong className="text-blue-500">React</strong>
           </h1>
@@ -42,7 +44,7 @@ export function Subscribe() {
           </p>
         </div>
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">
+          <strong className="text-2xl mb-6 block text-center md:text-left">
             Subscribe now! It's free!
           </strong>
           <form
@@ -51,13 +53,13 @@ export function Subscribe() {
           >
             <input
               type="text"
-              className="bg-gray-900 rounded px-5 h-14"
+              className="bg-gray-900 rounded px-2 h-10 md:px-5 md:h-14"
               placeholder="Your full name"
               onChange={(event) => setName(event.target.value)}
             />
             <input
               type="email"
-              className="bg-gray-900 rounded px-5 h-14"
+              className="bg-gray-900 rounded px-2 h-10 md:px-5 md:h-14"
               placeholder="Your email"
               onChange={(event) => setEmail(event.target.value)}
             />
@@ -65,14 +67,14 @@ export function Subscribe() {
             <button
               type="submit"
               disabled={loading}
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+              className="mt-4 bg-green-500 uppercase py-2 md:py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
               Get my spot
             </button>
           </form>
         </div>
       </div>
-      <img src={MockImage} alt="" className="mt-10" />
+      <img src={MockImage} alt="" className="mt-0 flex-shrink-0" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      screens: {
+        xs: "300px",
+      },
       backgroundImage: {
         blur: "url(/src/assets/blur-background.png)",
       },
